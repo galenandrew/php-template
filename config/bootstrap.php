8114dd05a275e76a66c929862f531b4dbd79ca66
Andrew Turner <galen.turner@me.com>
2012-01-03 10:38:25 -0600

    static html version

--- config/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once(BASE_ROOT.'config/config.php');
-if(file_exists(PHP_SCRIPTS.'functions.php')) include_once(PHP_SCRIPTS.'functions.php');
-if(defined('DB_REQUIRED') && DB_REQUIRED) include_once(CONFIG_PATH.'db.php');
-include_once(CONFIG_PATH.'routes.php');
--- templates/footer.php
+++
@@ -1,9 +0,0 @@
-	</div>
-	<!-- End Content -->
-	<footer>
-		<p>&copy; Company 2011</p>
-	</footer>
-</div>
-<!-- End Container -->
-</body>
-</html>
