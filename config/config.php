5c0ee0a63bebfb000e741788f00f942f98eee51b
Andrew Turner <galen.turner@me.com>
2012-10-01 00:09:25 -0500

    restructured configs

--- config/config.php
+++ config/config.php
@@ -9,23 +9,25 @@ error_reporting(0); ini_set('display_errors', 0);
  */
 //---> Paths
 define('DS', 					DIRECTORY_SEPARATOR);
-define('CONFIG_PATH',			BASE_ROOT.'config'.DS);
-define('TEMPLATE_PATH', 		BASE_ROOT.'templates'.DS);
-define('CLASSES', 				BASE_ROOT.'classes'.DS);
-define('CONTROLLERS', 			BASE_ROOT.'controllers'.DS);
-define('PAGES', 				BASE_ROOT.'pages'.DS);
-define('PHP_SCRIPTS', 			BASE_ROOT.'scripts'.DS.'php'.DS);
+define('CONFIG_PATH',			BASE_ROOT . 'config' . DS);
+define('TEMPLATE_PATH', 		BASE_ROOT . 'templates' . DS);
+define('CLASSES', 				BASE_ROOT . 'classes' . DS);
+define('CONTROLLERS', 			BASE_ROOT . 'controllers' . DS);
+define('PAGES', 				BASE_ROOT . 'pages' . DS);
+define('PHP_SCRIPTS', 			BASE_ROOT . 'scripts' . DS . 'php' . DS);
 //---> Files
-define('ERROR_404_CONTROLLER',	CONTROLLERS.'error_404.php');
-define('ERROR_404_PAGE',		PAGES.'error_404.php');
-define('ERROR_DB_CONTROLLER',	CONTROLLERS.'error_db.php');
-define('ERROR_DB_PAGE',			PAGES.'error_db.php');
+define('ERROR_404_CONTROLLER',	CONTROLLERS . 'error_404.php');
+define('ERROR_404_PAGE',		PAGES . 'error_404.php');
+define('ERROR_DB_CONTROLLER',	CONTROLLERS . 'error_db.php');
+define('ERROR_DB_PAGE',			PAGES . 'error_db.php');
 //---> Other
 define('DB_REQUIRED', 			FALSE);
 define('DBUG', 					FALSE);
 define('BASE_URL',				$_SERVER['HTTP_HOST']);
 define('REQ', 					isset($_GET['req']) ? $_GET['req']:'');
 define('REQ_URI', 				isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI']:'/');
+define('ASYNC', 				isset($_GET['async_req']) ? TRUE : FALSE);
+define('ROUTES', 				!isset($_GET['no_routes']) || !$_GET['no_routes'] ? TRUE : FALSE);
 
 /**
  *	Autoload Classes
@@ -33,29 +35,14 @@ define('REQ_URI', 				isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI']:'
  *	@description	Autoloads classes when initiated (use of __autoload is discouraged by PHP.net)
  */
 spl_autoload_register(function($class_name) { 
-	$class_file = CLASSES.strtolower($class_name).'.class.php';
+	$class_file = CLASSES . strtolower($class_name) . '.class.php';
 	if(file_exists($class_file)) { 
 		require_once($class_file);
 	} 
 }); 
 
-/**
- *	Auto-Detect Environment
- *	
- *	@description	Auto-detects environment based on URL
- */
-switch(BASE_URL) {
-	case strpos(BASE_URL, '.local') !== FALSE:
-		if(!defined('ENVR')) define('ENVR', 'local');
-	break;
-	case strpos(BASE_URL, 'dev.') !== FALSE:
-		if(!defined('ENVR')) define('ENVR', 'development');
-	break;
-	case strpos(BASE_URL, 'staging.') !== FALSE:
-		if(!defined('ENVR')) define('ENVR', 'staging');
-	break;
-	default: 
-	case strpos(BASE_URL, 'www.') !== FALSE:
-		if(!defined('ENVR')) define('ENVR', 'production');
-	break;
-}
+//---> Include ENVR scripts
+if(file_exists(CONFIG_PATH . 'config.envr.php')) include_once(CONFIG_PATH . 'config.envr.php');
+
+//---> Include DB scripts if needed
+if(defined('DB_REQUIRED') && DB_REQUIRED) include_once(CONFIG_PATH . 'db.php');
--- config/db.php
+++ config/db.php
@@ -7,10 +7,14 @@
 switch(ENVR) {
 	default: 
 	case 'local':
-		define('DB_HOST', 		'localhost');
-		define('DB_NAME', 		'database');
-		define('DB_USER', 		'username');
-		define('DB_PASSWORD', 	'password');
+		if(file_exists(CONFIG_PATH . 'db.local.php')) {
+			include_once(CONFIG_PATH . 'db.local.php');
+		} else {
+			define('DB_HOST', 		'localhost');
+			define('DB_NAME', 		'database');
+			define('DB_USER', 		'username');
+			define('DB_PASSWORD', 	'password');
+		}
 	break;
 	case 'development':
 		define('DB_HOST', 		'localhost');
@@ -33,6 +37,6 @@ switch(ENVR) {
 }
 
 //---> Connect to DB
-$dbsvr = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-define('DB_CONNECTED', (!$dbsvr) ? FALSE : TRUE);
-if(DB_CONNECTED) mysql_select_db(DB_NAME, $dbsvr);
+$db = new DB_Utility();
+define('DB_CONNECTED', (!$db->connected) ? FALSE : TRUE);
+// use $db->dbsvr or ($this->dbsvr in inherited classes) for connection
